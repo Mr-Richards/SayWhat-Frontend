0936f75c270d156bdba15371204648dfd7a8a28c
Alex Price-Richards <alexpricerichards@gmail.com>
2022-06-09 10:03:45 +0200

    Feat: access to camera with flip functionality

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>Please allow access to camera in settings</Text>;
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          autoFocus="on"
+        >
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.flipButton}
+              onPress={() => {
+                setType(
+                  type === CameraType.back ? CameraType.front : CameraType.back,
+                );
+                console.log('Flipped');
+              }}
+            >
+              <Text style={styles.text}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    margin: 20,
+  },
+  flipButton: {
+    flex: 1,
+    alignSelf: 'flex-start',
+    justifyContent: 'flex-end',
+    marginBottom: '5%',
+    marginLeft: '5%',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
